7501fd444152b257c628bf06c203c9b54df443d8
Marvin_zhang <8072064+marvinzhang@user.noreply.gitee.com>
2021-07-22 11:08:02 +0800

    优化demo_beta

--- China/demo/app/src/main/java/com/anythink/custom/adapter/PangleInitManager.java
+++ China/demo/app/src/main/java/com/anythink/custom/adapter/PangleInitManager.java
@@ -1,174 +1,174 @@
-/*
- * Copyright © 2018-2020 TopOn. All rights reserved.
- * https://www.toponad.com
- * Licensed under the TopOn SDK License Agreement
- * https://github.com/toponteam/TopOn-Android-SDK/blob/master/LICENSE
- */
-
-package com.anythink.custom.adapter;
-
-import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-
-import com.anythink.core.api.ATInitMediation;
-import com.bytedance.sdk.openadsdk.TTAdConfig;
-import com.bytedance.sdk.openadsdk.TTAdConstant;
-import com.bytedance.sdk.openadsdk.TTAdManager;
-import com.bytedance.sdk.openadsdk.TTAdSdk;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-public class PangleInitManager extends ATInitMediation {
-    public static final String TAG = PangleInitManager.class.getSimpleName();
-
-    private static PangleInitManager sInstance;
-    private Handler mHandler;
-    private boolean mIsOpenDirectDownload;
-
-    private boolean mHasInit;
-    private AtomicBoolean mIsIniting;
-    private List<PangleInitManager.InitCallback> mListeners;
-    private final Object mLock = new Object();
-
-    private PangleInitManager() {
-        mHandler = new Handler(Looper.getMainLooper());
-        mIsOpenDirectDownload = true;
-        mIsIniting = new AtomicBoolean(false);
-    }
-
-    public synchronized static PangleInitManager getInstance() {
-        if (sInstance == null) {
-            sInstance = new PangleInitManager();
-        }
-        return sInstance;
-    }
-
-
-    @Override
-    public synchronized void initSDK(Context context, Map<String, Object> serviceExtras) {
-        initSDK(context, serviceExtras, null);
-    }
-
-    public void initSDK(final Context context, Map<String, Object> serviceExtras, final PangleInitManager.InitCallback callback) {
-
-        if (TTAdSdk.isInitSuccess() || mHasInit) {
-            if (callback != null) {
-                callback.onSuccess();
-            }
-            return;
-        }
-
-        synchronized (mLock) {
-
-            if (mIsIniting.get()) {
-                if (callback != null) {
-                    mListeners.add(callback);
-                }
-                return;
-            }
-
-            if (mListeners == null) {
-                mListeners = new ArrayList<>();
-            }
-
-            mIsIniting.set(true);
-        }
-
-        final String appId = (String) serviceExtras.get("app_id");
-
-        if (callback != null) {
-            mListeners.add(callback);
-        }
-
-        final int[] download;
-        if (mIsOpenDirectDownload) {
-            download = new int[]{
-                    TTAdConstant.NETWORK_STATE_MOBILE, TTAdConstant.NETWORK_STATE_2G, TTAdConstant.NETWORK_STATE_3G,
-                    TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_4G
-            };
-        } else {
-            download = new int[]{
-                    TTAdConstant.NETWORK_STATE_2G
-            };
-        }
-
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    TTAdSdk.init(context.getApplicationContext(), new TTAdConfig.Builder()
-                            .appId(appId)
-                            .useTextureView(true) //Use the TextureView control to play the video. The default is SurfaceView. When there are conflicts in SurfaceView, you can use TextureView
-                            .appName(context.getPackageManager().getApplicationLabel(context.getApplicationInfo()).toString())
-                            .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
-                            .directDownloadNetworkType(download) //Allow all network downloads
-                            .supportMultiProcess(false) //Whether to support multiple processes, true support
-                            .build(), new TTAdSdk.InitCallback() {
-                        @Override
-                        public void success() {
-                            mHasInit = true;
-
-                            callbackResult(true, null, null);
-                        }
-
-                        @Override
-                        public void fail(int errorCode, String errorMsg) {
-                            callbackResult(false, errorCode + "", errorMsg);
-                        }
-                    });
-
-                } catch (Throwable e) {
-                    callbackResult(false, "", e.getMessage());
-                }
-            }
-        });
-    }
-
-    private void callbackResult(boolean success, String errorCode, String errorMsg) {
-        synchronized (mLock) {
-            int size = mListeners.size();
-            PangleInitManager.InitCallback initListener;
-            for (int i = 0; i < size; i++) {
-                initListener = mListeners.get(i);
-                if (initListener != null) {
-                    if (success) {
-                        initListener.onSuccess();
-                    } else {
-                        initListener.onError(errorCode, errorMsg);
-                    }
-                }
-            }
-            mListeners.clear();
-
-            mIsIniting.set(false);
-        }
-    }
-
-
-    interface InitCallback {
-        void onSuccess();
-
-        void onError(String errorCode, String errorMsg);
-    }
-
-    @Override
-    public String getNetworkName() {
-        return "Pangle(Tiktok) Custom";
-    }
-
-
-    @Override
-    public String getNetworkVersion() {
-        try {
-            TTAdManager ttAdManager = TTAdSdk.getAdManager();
-            return ttAdManager.getSDKVersion();
-        } catch (Throwable e) {
-
-        }
-        return "";
-    }
-}
+///*
+// * Copyright © 2018-2020 TopOn. All rights reserved.
+// * https://www.toponad.com
+// * Licensed under the TopOn SDK License Agreement
+// * https://github.com/toponteam/TopOn-Android-SDK/blob/master/LICENSE
+// */
+//
+//package com.anythink.custom.adapter;
+//
+//import android.content.Context;
+//import android.os.Handler;
+//import android.os.Looper;
+//
+//import com.anythink.core.api.ATInitMediation;
+//import com.bytedance.sdk.openadsdk.TTAdConfig;
+//import com.bytedance.sdk.openadsdk.TTAdConstant;
+//import com.bytedance.sdk.openadsdk.TTAdManager;
+//import com.bytedance.sdk.openadsdk.TTAdSdk;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.concurrent.atomic.AtomicBoolean;
+//
+//public class PangleInitManager extends ATInitMediation {
+//    public static final String TAG = PangleInitManager.class.getSimpleName();
+//
+//    private static PangleInitManager sInstance;
+//    private Handler mHandler;
+//    private boolean mIsOpenDirectDownload;
+//
+//    private boolean mHasInit;
+//    private AtomicBoolean mIsIniting;
+//    private List<PangleInitManager.InitCallback> mListeners;
+//    private final Object mLock = new Object();
+//
+//    private PangleInitManager() {
+//        mHandler = new Handler(Looper.getMainLooper());
+//        mIsOpenDirectDownload = true;
+//        mIsIniting = new AtomicBoolean(false);
+//    }
+//
+//    public synchronized static PangleInitManager getInstance() {
+//        if (sInstance == null) {
+//            sInstance = new PangleInitManager();
+//        }
+//        return sInstance;
+//    }
+//
+//
+//    @Override
+//    public synchronized void initSDK(Context context, Map<String, Object> serviceExtras) {
+//        initSDK(context, serviceExtras, null);
+//    }
+//
+//    public void initSDK(final Context context, Map<String, Object> serviceExtras, final PangleInitManager.InitCallback callback) {
+//
+//        if (TTAdSdk.isInitSuccess() || mHasInit) {
+//            if (callback != null) {
+//                callback.onSuccess();
+//            }
+//            return;
+//        }
+//
+//        synchronized (mLock) {
+//
+//            if (mIsIniting.get()) {
+//                if (callback != null) {
+//                    mListeners.add(callback);
+//                }
+//                return;
+//            }
+//
+//            if (mListeners == null) {
+//                mListeners = new ArrayList<>();
+//            }
+//
+//            mIsIniting.set(true);
+//        }
+//
+//        final String appId = (String) serviceExtras.get("app_id");
+//
+//        if (callback != null) {
+//            mListeners.add(callback);
+//        }
+//
+//        final int[] download;
+//        if (mIsOpenDirectDownload) {
+//            download = new int[]{
+//                    TTAdConstant.NETWORK_STATE_MOBILE, TTAdConstant.NETWORK_STATE_2G, TTAdConstant.NETWORK_STATE_3G,
+//                    TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_4G
+//            };
+//        } else {
+//            download = new int[]{
+//                    TTAdConstant.NETWORK_STATE_2G
+//            };
+//        }
+//
+//        mHandler.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    TTAdSdk.init(context.getApplicationContext(), new TTAdConfig.Builder()
+//                            .appId(appId)
+//                            .useTextureView(true) //Use the TextureView control to play the video. The default is SurfaceView. When there are conflicts in SurfaceView, you can use TextureView
+//                            .appName(context.getPackageManager().getApplicationLabel(context.getApplicationInfo()).toString())
+//                            .titleBarTheme(TTAdConstant.TITLE_BAR_THEME_DARK)
+//                            .directDownloadNetworkType(download) //Allow all network downloads
+//                            .supportMultiProcess(false) //Whether to support multiple processes, true support
+//                            .build(), new TTAdSdk.InitCallback() {
+//                        @Override
+//                        public void success() {
+//                            mHasInit = true;
+//
+//                            callbackResult(true, null, null);
+//                        }
+//
+//                        @Override
+//                        public void fail(int errorCode, String errorMsg) {
+//                            callbackResult(false, errorCode + "", errorMsg);
+//                        }
+//                    });
+//
+//                } catch (Throwable e) {
+//                    callbackResult(false, "", e.getMessage());
+//                }
+//            }
+//        });
+//    }
+//
+//    private void callbackResult(boolean success, String errorCode, String errorMsg) {
+//        synchronized (mLock) {
+//            int size = mListeners.size();
+//            PangleInitManager.InitCallback initListener;
+//            for (int i = 0; i < size; i++) {
+//                initListener = mListeners.get(i);
+//                if (initListener != null) {
+//                    if (success) {
+//                        initListener.onSuccess();
+//                    } else {
+//                        initListener.onError(errorCode, errorMsg);
+//                    }
+//                }
+//            }
+//            mListeners.clear();
+//
+//            mIsIniting.set(false);
+//        }
+//    }
+//
+//
+//    interface InitCallback {
+//        void onSuccess();
+//
+//        void onError(String errorCode, String errorMsg);
+//    }
+//
+//    @Override
+//    public String getNetworkName() {
+//        return "Pangle(Tiktok) Custom";
+//    }
+//
+//
+//    @Override
+//    public String getNetworkVersion() {
+//        try {
+//            TTAdManager ttAdManager = TTAdSdk.getAdManager();
+//            return ttAdManager.getSDKVersion();
+//        } catch (Throwable e) {
+//
+//        }
+//        return "";
+//    }
+//}
--- China/demo/app/src/main/java/com/anythink/custom/adapter/PangleSplashAdapter.java
+++ China/demo/app/src/main/java/com/anythink/custom/adapter/PangleSplashAdapter.java
@@ -1,202 +1,202 @@
-/*
- * Copyright © 2018-2020 TopOn. All rights reserved.
- * https://www.toponad.com
- * Licensed under the TopOn SDK License Agreement
- * https://github.com/toponteam/TopOn-Android-SDK/blob/master/LICENSE
- */
-
-package com.anythink.custom.adapter;
-
-import android.app.Activity;
-import android.content.Context;
-import android.text.TextUtils;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.anythink.core.api.ATAdConst;
-import com.anythink.splashad.unitgroup.api.CustomSplashAdapter;
-import com.bytedance.sdk.openadsdk.AdSlot;
-import com.bytedance.sdk.openadsdk.TTAdManager;
-import com.bytedance.sdk.openadsdk.TTAdNative;
-import com.bytedance.sdk.openadsdk.TTAdSdk;
-import com.bytedance.sdk.openadsdk.TTSplashAd;
-
-import java.util.Map;
-
-public class PangleSplashAdapter extends CustomSplashAdapter implements TTSplashAd.AdInteractionListener {
-    private final String TAG = getClass().getSimpleName();
-
-    String appId = "";
-    String slotId = "";
-    String personalizedTemplate = "";
-
-    TTSplashAd splashAd;
-
-    @Override
-    public void loadCustomNetworkAd(final Context context, Map<String, Object> serverExtra, final Map<String, Object> localExtra) {
-        if (serverExtra.containsKey("app_id") && serverExtra.containsKey("slot_id")) {
-            appId = (String) serverExtra.get("app_id");
-            slotId = (String) serverExtra.get("slot_id");
-
-        } else {
-            if (mLoadListener != null) {
-                mLoadListener.onAdLoadError("", "app_id or slot_id is empty!");
-            }
-            return;
-        }
-
-        personalizedTemplate = "0";
-        if (serverExtra.containsKey("personalized_template")) {
-            personalizedTemplate = (String) serverExtra.get("personalized_template");
-        }
-
-        PangleInitManager.getInstance().initSDK(context, serverExtra, new PangleInitManager.InitCallback() {
-            @Override
-            public void onSuccess() {
-                startLoad(context, localExtra);
-            }
-
-            @Override
-            public void onError(String errorCode, String errorMsg) {
-                if (mLoadListener != null) {
-                    mLoadListener.onAdLoadError(errorCode, errorMsg);
-                }
-            }
-        });
-    }
-
-    private void startLoad(Context context, Map<String, Object> localExtra) {
-        TTAdManager ttAdManager = TTAdSdk.getAdManager();
-
-        final TTAdNative mTTAdNative = ttAdManager.createAdNative(context);//baseContext is recommended for activity
-        final AdSlot.Builder adSlotBuilder = new AdSlot.Builder().setCodeId(slotId);
-
-        int width = 0;
-        int height = 0;
-        try {
-            if (localExtra.containsKey(ATAdConst.KEY.AD_WIDTH)) {
-                width = Integer.parseInt(localExtra.get(ATAdConst.KEY.AD_WIDTH).toString());
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
-        try {
-            if (localExtra.containsKey(ATAdConst.KEY.AD_HEIGHT)) {
-                height = Integer.parseInt(localExtra.get(ATAdConst.KEY.AD_HEIGHT).toString());
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
-        adSlotBuilder.setImageAcceptedSize(width, height); //Must be set
-
-        if (TextUtils.equals("1", personalizedTemplate)) {// Native Express
-            adSlotBuilder.setExpressViewAcceptedSize(px2dip(context, width), px2dip(context, height));
-        }
-
-        postOnMainThread(new Runnable() {
-            @Override
-            public void run() {
-                AdSlot adSlot = adSlotBuilder.build();
-                mTTAdNative.loadSplashAd(adSlot, new TTAdNative.SplashAdListener() {
-                    @Override
-                    public void onError(int i, String s) {
-                        if (mLoadListener != null) {
-                            mLoadListener.onAdLoadError(i + "", s);
-                        }
-                    }
-
-                    @Override
-                    public void onTimeout() {
-                        if (mLoadListener != null) {
-                            mLoadListener.onAdLoadError("", "onTimeout");
-                        }
-                    }
-
-                    @Override
-                    public void onSplashAdLoad(TTSplashAd ttSplashAd) {
-                        splashAd = ttSplashAd;
-                        if (mLoadListener != null) {
-                            mLoadListener.onAdCacheLoaded();
-                        }
-                    }
-                }, mFetchAdTimeout);
-            }
-        });
-
-    }
-
-    @Override
-    public boolean isAdReady() {
-        return splashAd != null;
-    }
-
-    @Override
-    public void show(Activity activity, ViewGroup container) {
-        if (splashAd != null) {
-            splashAd.setSplashInteractionListener(PangleSplashAdapter.this);
-            View splashView = splashAd.getSplashView();
-            if (splashView != null) {
-                container.addView(splashView);
-            }
-        }
-
-    }
-
-    @Override
-    public String getNetworkName() {
-        return PangleInitManager.getInstance().getNetworkName();
-    }
-
-    @Override
-    public void destory() {
-
-    }
-
-    @Override
-    public String getNetworkPlacementId() {
-        return slotId;
-    }
-
-    @Override
-    public String getNetworkSDKVersion() {
-        return PangleInitManager.getInstance().getNetworkVersion();
-    }
-
-    @Override
-    public void onAdClicked(View view, int i) {
-        if (mImpressionListener != null) {
-            mImpressionListener.onSplashAdClicked();
-        }
-
-    }
-
-    @Override
-    public void onAdShow(View view, int i) {
-        if (mImpressionListener != null) {
-            mImpressionListener.onSplashAdShow();
-        }
-
-    }
-
-    @Override
-    public void onAdSkip() {
-        if (mImpressionListener != null) {
-            mImpressionListener.onSplashAdDismiss();
-        }
-    }
-
-    @Override
-    public void onAdTimeOver() {
-        if (mImpressionListener != null) {
-            mImpressionListener.onSplashAdDismiss();
-        }
-    }
-
-    private static int px2dip(Context context, float pxValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (pxValue / (scale <= 0 ? 1 : scale) + 0.5f);
-    }
-
-}
+///*
+// * Copyright © 2018-2020 TopOn. All rights reserved.
+// * https://www.toponad.com
+// * Licensed under the TopOn SDK License Agreement
+// * https://github.com/toponteam/TopOn-Android-SDK/blob/master/LICENSE
+// */
+//
+//package com.anythink.custom.adapter;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.text.TextUtils;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.anythink.core.api.ATAdConst;
+//import com.anythink.splashad.unitgroup.api.CustomSplashAdapter;
+//import com.bytedance.sdk.openadsdk.AdSlot;
+//import com.bytedance.sdk.openadsdk.TTAdManager;
+//import com.bytedance.sdk.openadsdk.TTAdNative;
+//import com.bytedance.sdk.openadsdk.TTAdSdk;
+//import com.bytedance.sdk.openadsdk.TTSplashAd;
+//
+//import java.util.Map;
+//
+//public class PangleSplashAdapter extends CustomSplashAdapter implements TTSplashAd.AdInteractionListener {
+//    private final String TAG = getClass().getSimpleName();
+//
+//    String appId = "";
+//    String slotId = "";
+//    String personalizedTemplate = "";
+//
+//    TTSplashAd splashAd;
+//
+//    @Override
+//    public void loadCustomNetworkAd(final Context context, Map<String, Object> serverExtra, final Map<String, Object> localExtra) {
+//        if (serverExtra.containsKey("app_id") && serverExtra.containsKey("slot_id")) {
+//            appId = (String) serverExtra.get("app_id");
+//            slotId = (String) serverExtra.get("slot_id");
+//
+//        } else {
+//            if (mLoadListener != null) {
+//                mLoadListener.onAdLoadError("", "app_id or slot_id is empty!");
+//            }
+//            return;
+//        }
+//
+//        personalizedTemplate = "0";
+//        if (serverExtra.containsKey("personalized_template")) {
+//            personalizedTemplate = (String) serverExtra.get("personalized_template");
+//        }
+//
+//        PangleInitManager.getInstance().initSDK(context, serverExtra, new PangleInitManager.InitCallback() {
+//            @Override
+//            public void onSuccess() {
+//                startLoad(context, localExtra);
+//            }
+//
+//            @Override
+//            public void onError(String errorCode, String errorMsg) {
+//                if (mLoadListener != null) {
+//                    mLoadListener.onAdLoadError(errorCode, errorMsg);
+//                }
+//            }
+//        });
+//    }
+//
+//    private void startLoad(Context context, Map<String, Object> localExtra) {
+//        TTAdManager ttAdManager = TTAdSdk.getAdManager();
+//
+//        final TTAdNative mTTAdNative = ttAdManager.createAdNative(context);//baseContext is recommended for activity
+//        final AdSlot.Builder adSlotBuilder = new AdSlot.Builder().setCodeId(slotId);
+//
+//        int width = 0;
+//        int height = 0;
+//        try {
+//            if (localExtra.containsKey(ATAdConst.KEY.AD_WIDTH)) {
+//                width = Integer.parseInt(localExtra.get(ATAdConst.KEY.AD_WIDTH).toString());
+//            }
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            if (localExtra.containsKey(ATAdConst.KEY.AD_HEIGHT)) {
+//                height = Integer.parseInt(localExtra.get(ATAdConst.KEY.AD_HEIGHT).toString());
+//            }
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//        }
+//
+//        adSlotBuilder.setImageAcceptedSize(width, height); //Must be set
+//
+//        if (TextUtils.equals("1", personalizedTemplate)) {// Native Express
+//            adSlotBuilder.setExpressViewAcceptedSize(px2dip(context, width), px2dip(context, height));
+//        }
+//
+//        postOnMainThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                AdSlot adSlot = adSlotBuilder.build();
+//                mTTAdNative.loadSplashAd(adSlot, new TTAdNative.SplashAdListener() {
+//                    @Override
+//                    public void onError(int i, String s) {
+//                        if (mLoadListener != null) {
+//                            mLoadListener.onAdLoadError(i + "", s);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onTimeout() {
+//                        if (mLoadListener != null) {
+//                            mLoadListener.onAdLoadError("", "onTimeout");
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onSplashAdLoad(TTSplashAd ttSplashAd) {
+//                        splashAd = ttSplashAd;
+//                        if (mLoadListener != null) {
+//                            mLoadListener.onAdCacheLoaded();
+//                        }
+//                    }
+//                }, mFetchAdTimeout);
+//            }
+//        });
+//
+//    }
+//
+//    @Override
+//    public boolean isAdReady() {
+//        return splashAd != null;
+//    }
+//
+//    @Override
+//    public void show(Activity activity, ViewGroup container) {
+//        if (splashAd != null) {
+//            splashAd.setSplashInteractionListener(PangleSplashAdapter.this);
+//            View splashView = splashAd.getSplashView();
+//            if (splashView != null) {
+//                container.addView(splashView);
+//            }
+//        }
+//
+//    }
+//
+//    @Override
+//    public String getNetworkName() {
+//        return PangleInitManager.getInstance().getNetworkName();
+//    }
+//
+//    @Override
+//    public void destory() {
+//
+//    }
+//
+//    @Override
+//    public String getNetworkPlacementId() {
+//        return slotId;
+//    }
+//
+//    @Override
+//    public String getNetworkSDKVersion() {
+//        return PangleInitManager.getInstance().getNetworkVersion();
+//    }
+//
+//    @Override
+//    public void onAdClicked(View view, int i) {
+//        if (mImpressionListener != null) {
+//            mImpressionListener.onSplashAdClicked();
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onAdShow(View view, int i) {
+//        if (mImpressionListener != null) {
+//            mImpressionListener.onSplashAdShow();
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onAdSkip() {
+//        if (mImpressionListener != null) {
+//            mImpressionListener.onSplashAdDismiss();
+//        }
+//    }
+//
+//    @Override
+//    public void onAdTimeOver() {
+//        if (mImpressionListener != null) {
+//            mImpressionListener.onSplashAdDismiss();
+//        }
+//    }
+//
+//    private static int px2dip(Context context, float pxValue) {
+//        final float scale = context.getResources().getDisplayMetrics().density;
+//        return (int) (pxValue / (scale <= 0 ? 1 : scale) + 0.5f);
+//    }
+//
+//}
